7860e6265209937b6bb5127860271f2deb7e6dad
Theo KALIFA <kalifa.theo@gmail.com>
2022-05-12 10:59:52 +0200

    Ajout d'un menu de selection du mode (compression/decompression)
    Il reste l'appel de fonction par rapport au mode choisi voir d'autre demande comme un nom de fichier par exemple.

--- header.h
+++ header.h
@@ -1,3 +1,22 @@
 #include<stdlib.h>
 #include<stdio.h>
 #include<math.h>
+
+/* ________________________________
+Prototype main.c function
+________________________________ */
+
+/* The default main function
+*/
+int main();
+
+/* Read a char from the user, the function check tif the value is allow
+ *
+ * minVal : the minimal value allow
+ * maxVal : the maximal value allox
+ * message : the message to display
+ *
+ * Return an int containing the value of the char given by the user
+*/
+int getChar(int minVal, int maxVal, char* message);
+
--- main.c
+++ main.c
@@ -1,7 +1,52 @@
 #include"header.h"
 
+#define MODE_CHOICE_TEXT "\nVeuillez choisir un mode de fonctionnement\n1) Compression\n2) Decompression\nLes réponses possibles sont 1, 2 : "
+
+int getChar(int minVal, int maxVal, char* message){
+	int readChar = 0; //Contain a char when fgetc is use
+	int tmp = 0;      //A temp var, use for store data a short amount of time
+	char check = 0;    //Use to make a check in a if or a while
+	
+	do{
+		printf("%s", message);
+		
+		readChar = fgetc(stdin);
+		
+		//If the input is juste \n, we need to put a default value to tmp
+		if(readChar!='\n'){
+			tmp = fgetc(stdin);
+		}else{
+			tmp = '\n';
+		}
+		
+		//Check if the answer has an error (not valide char or to many char)
+		if( !(minVal <= readChar && readChar <= maxVal && (tmp == '\n' || tmp == EOF)) ){
+			printf("\n/!\\Merci de bien vouloir saisir une réponse valide/!\\\n\n\n");
+			check = 0;
+			
+			//We need to clear the buffer of fgetc if we have other char
+			while(tmp != '\n' && tmp != EOF){
+				tmp=fgetc(stdin);
+			}
+		}else{
+			check = 1;
+		}
+		
+	}while(check == 0);
+	
+	return readChar;
+}
 
 int main(){
-	printf("Hello Wolrd");	
+	printf("Interface de paramétrage algorithme EVA\n");
+	
+	char input = getChar('1', '2', MODE_CHOICE_TEXT); //Getting the user input
+	
+	//Compression mode
+	if(input == 1){
+		
+	}
+	
+	
 	return 0;
 }
