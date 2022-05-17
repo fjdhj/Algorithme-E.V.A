98ebc7b8cd11c9231b2aa718bf3449568da75a49
alexandregrisez <104682287+alexandregrisez@users.noreply.github.com>
2022-05-17 11:36:22 +0200

    compression a l'air de fonctionner correctement

--- fonctions.c
+++ fonctions.c
@@ -1,3 +1,4 @@
+#include <stdint.h>
 #include "ppm_lib.c"
 #define EVA_BLK_SAME 192
 #define EVA_BLK_INDEX 0
@@ -8,41 +9,92 @@
 
 // #  &  { }  ~  [] \  |
 
-void write(FILE* file,int val){                //Writes an int value inside a binary file. Takes the file structure and the written value as parameters.
-	fwrite(&val,sizeof(int),1,file);
+unsigned int reverseint(unsigned int a){ 
+	unsigned int reversed;
+	uint8_t *n1,*n2;
+	n1=(uint8_t *) &a;
+	n2=(uint8_t *) &reversed;
+	n2[0]=n1[3];
+	n2[1]=n1[2];
+	n2[2]=n1[1];
+	n2[3]=n1[0];
+	return reversed;
 }
 
-void compression(char* filename){			//Takes the name of a ppm file as a parameter, creates a new compressed binary file named "compressed/name of file/" using the EVA algorithm method.
-    char* name=sprintf("compressed%s",filename);
-    int EVA_BLK_DEBUG[10];
-    int cache[64];
+unsigned short reverseshort(unsigned short a){ 
+	unsigned short reversed;
+	uint8_t *n1,*n2;
+	n1=(uint8_t *) &a;
+	n2=(uint8_t *) &reversed;
+	n2[0]=n1[1];
+	n2[1]=n1[0];
+	return reversed;
+}
+
+
+void write4blocks(FILE* file,unsigned int val){                //Writes an int value inside a binary file. Takes the file structure and the written value as parameters.
+    unsigned int reversed=reverseint(val);
+    if(fwrite(&reversed,4,1,file)!=1){
+        printf("\nThere was a problem during compression.");
+        exit(-1);
+    }
+}
+
+void write2blocks(FILE* file,unsigned short val){                //Writes an int value inside a binary file. Takes the file structure and the written value as parameters.
+    unsigned short reversed=reverseshort(val);
+    if(fwrite(&reversed,2,1,file)!=1){
+        printf("\nThere was a problem during compression.");
+        exit(-1);
+    }
+}
+
+void write1blocks(FILE* file,char val){                //Writes an int value inside a binary file. Takes the file structure and the written value as parameters.
+    if(fwrite(&val,1,1,file)!=1){
+        printf("\nThere was a problem during compression.");
+        exit(-1);
+    }
+}
+
+void compression(char* filename){			//Takes the name of a ppm file as a parameter, creates a new compressed binary file using the EVA algorithm method.
+    char* name;
+    int cache[64],cacheindex=0;
     int previouspixel,currentpixel,output;
     int i,j,width,height,range,colors;
-    int samepixels=0,index,diffr,diffb,diffg,diffrg,diffbg;
+    int samepixels,index,diffr,diffb,diffg,diffrg,diffbg;
     for(i=0;i<64;i++){
           cache[i]=0;
     }
-    PPM_IMG* old=ppmOpen("Image.ppm");
+
+    printf("\nFILE %s : Give a name to the compressed file.",filename);
+    while(scanf("%s",name)!=1)){ 
+	printf("\nInvalid name.");
+    }
+
+    PPM_IMG* old=ppmOpen("ppm/filename");
     if(!old){
         printf("\n There was a problem.");
+        exit(-1);
     }
     width=ppmGetWidth(old);
     height=ppmGetHeight(old);
     range=ppmGetRange(old);
     colors=ppmGetColors(old);
-    FILE* new=fopen(name,"wb+");
+
+    FILE* new=fopen("imagecomprimee","wb+");
     if(!new){
         printf("\n There was a problem.");
+        exit(-1);
     }
-
+    previouspixel=-1;
+    samepixels=0;
     for(i=0;i<height;i++){
         for(j=0;j<width;j++){
             output=0;
-            currentpixel=ppmRead(old,i,j);
+            currentpixel=ppmRead(old,j,i);
             //Step 1
             if(currentpixel==previouspixel){
-                if(EVA_BLK_SAME==192+63){
-                    write(new,EVA_BLK_SAME-1);
+                if(samepixels==63){
+                    write1blocks(new,EVA_BLK_SAME+samepixels-1);
                     samepixels=0;
                     output=1;
                 }
@@ -51,48 +103,49 @@ void compression(char* filename){			//Takes the name of a ppm file as a paramete
 		    output=1;
                 }
             }
-            else if(previouspixel!=currentpixel && EVA_BLK_SAME>192){
-                write(new,EVA_BLK_SAME-1);
+            if(previouspixel!=currentpixel && samepixels>0){
+                write1blocks(new,EVA_BLK_SAME+samepixels-1);
                 samepixels=0;
-                output=1;
+                output=0;
             }
             //Step 2
-            else if(output==0){
+            if(output==0){
                  index=(3*red(currentpixel)+5*green(currentpixel)+7*blue(currentpixel))%64;
                  if(currentpixel==cache[index]){
-                    write(new,EVA_BLK_INDEX+index);
+                    write1blocks(new,EVA_BLK_INDEX+index);
                     output=1;
                 }
             }
             //Step 3
-            else if(output==0){
+            if(output==0){
                 diffr=red(currentpixel)-red(previouspixel);
                 diffg=green(currentpixel)-green(previouspixel);
                 diffb=blue(currentpixel)-blue(previouspixel);
-                if(diffr>=-2 && diffr<=1 && diffg>=-2 && diffg<=1 && diffb>=-2 && diffb<=1){
-                    write(new,EVA_BLK_DIFF+diffb+2+4*(diffg+2)+16*(diffr+2));
+                if(((diffr>=-2 && diffr<=1) && diffg>=-2) && ((diffg<=1 && diffb>=-2) && diffb<=1)){
+                    write1blocks(new,EVA_BLK_DIFF+diffb+2+4*(diffg+2)+16*(diffr+2));
                     output=1;
                 }
             }
             //Step 4
-            else if(output==0){
+            if(output==0){
                 if(diffg>=-32 && diffg<=31){
                     diffrg=diffr-diffg;
                     diffbg=diffb-diffg;
-                    if(diffrg>=-8 && diffrg<=7 && diffbg>=-8 && diffbg<=7){
-                        write(new,EVA_BLK_LUMA+diffbg+8+16*(diffrg+8)+256*(diffg+32));
+                    if((diffrg>=-8 && diffrg<=7) && (diffbg>=-8 && diffbg<=7)){
+                        write2blocks(new,EVA_BLK_LUMA+diffbg+8+16*(diffrg+8)+256*(diffg+32));
                         output=1;
                     }
                 }
             }
             //Step 5
-            else if(output==0){
-                write(new,EVA_BLK_RGB+blue(currentpixel)+256*green(currentpixel)+65536*red(currentpixel));
+            if(output==0){
+                write4blocks(new,EVA_BLK_RGB+blue(currentpixel)+256*green(currentpixel)+65536*red(currentpixel));
                 output=1;
             }
             //Step 6
             //Step 7
-            cache[j%64]=currentpixel;
+            cacheindex++;
+            cache[cacheindex%64]=currentpixel;
             previouspixel=currentpixel;
         }
     }
