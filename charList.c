c6d1e7e1197e363d4efddc29d64e3703ee1db17f
Theo KALIFA <kalifa.theo@gmail.com>
2022-05-21 11:25:01 +0200

    correction bogue CharList

--- charList.c
+++ charList.c
@@ -1,9 +1,9 @@
-#include "liste.h"
+#include "list.h"
 
 CharList* createCharList(){
 	CharList* list = malloc(sizeof(CharList));
 	if(list == NULL){
-		printf("[ERROR] A memory error occured. Can't create a new CharList. Program Exit.");
+		printf("[ERROR] A memory error occured. Can't create a new CharList. Program Exit.\n");
 		exit(-1);
 	}
 	list->next = NULL;
@@ -15,30 +15,40 @@ void displayCharList(CharList* list){
 	CharList* p1 = list;
 	
 	while(p1 != NULL){
-		id(p1->suivant == NULL){
+		if(p1->next == NULL){
 			printf("%c}", p1->val);
 		}else{
 			printf("%c, ",p1->val);
 		}
-		p1 = p1 -> suivant;
+		p1 = p1 -> next;
 	}
 }
 
-void getSize(CharList* list){
-	CharList p1 = list;
+int getSize(CharList* list){
+	CharList* p1 = list;
 	int count = 0;
 	while(p1 != NULL){
 		count++;
 		p1 = p1-> next;
 	}
+	
+	return count;
 }
 
-void addCharList(int index, char val, CharList* list){
+CharList* addCharList(int index, char val, CharList* list){
 	if(list == NULL){
-		printf("[ERROR] Can't add a element to NULL. Program Exit.");
+		printf("[ERROR] Can't add a element to NULL. Program Exit.\n");
 		exit(-1);
 	}
-	if(index == 0){
+	
+	int size = getSize(list);
+	
+	if(index + size + 1 < 0 || index > size){
+		printf("[ERROR] A memory error occured. Can't access to the index %d in the CharList. Program Exit.\n", index);
+		exit(-1);
+	}
+	
+	if(index == 0 || size+1 == -index){
 		CharList* p2 = createCharList();
 		p2->val = val;
 		p2->next = list;
@@ -46,16 +56,16 @@ void addCharList(int index, char val, CharList* list){
 		
 	}else{
 		if(index < 0){
-			index += getSize(list);
+			index += size;
 		}else{
 			index--;
 		}
 	
-		CharList p1 = list;
+		CharList* p1 = list;
 		for(int i = 0; i < index; i++){
-			p1 = p1-> next;
+			p1 = p1->next;
 			if(p1 == NULL){
-				printf("[ERROR] A memory error occured. Can't access to the index %d in the CharList. Program Exit.");
+				printf("[ERROR] A memory error occured. Can't access to the index %d in the CharList. Program Exit.\n", index);
 				exit(-1);
 			}
 		}
@@ -66,41 +76,101 @@ void addCharList(int index, char val, CharList* list){
 		p2->next = p1->next;
 		p1->next = p2;
 	}
+	return list;
 }
 
 void appendCharList(char val, CharList* list){
 	addCharList(-1, val, list);
 }
 
-void removeFirstCharList(CharList* list){
+CharList* removeFirstCharList(CharList* list){
 	if(list == NULL){
-		printf("[ERROR] Can't remove a element from NULL. Program Exit.");
+		printf("[ERROR] Can't remove a element from NULL. Program Exit.\n");
 		exit(-1);
 	}
+	
 	CharList* p1 = list;
 	list = list->next;
 	free(p1);
+	return list;
 }
 
-void removeLastCharList(CharList* list){
+CharList* removeLastCharList(CharList* list){
 	if(list == NULL){
-		printf("[ERROR] Can't remove a element from NULL. Program Exit.");
-		exit(-1);
+		printf("[ERROR] Can't remove a element from NULL. Program Exit.\n");
+		exit(-1);	
 	}
 	CharList* p1 = list;
-	while(p1->next->next != NULL){
-		p1 = p1->next;
+	
+	if(p1->next == NULL){
+		free(p1);
+		return NULL;
+	}else{
+		while(p1->next->next != NULL){
+			p1 = p1->next;
+		}
+		free(p1->next);
+		p1->next = NULL;
+		return list;
 	}
-	free(p1->next)
 }
 
 void destroyCharList(CharList* list){
 	if(list == NULL){
-		printf("[ERROR] Can't remove a element from NULL. Program Exit.");
+		printf("[ERROR] Can't remove a element from NULL. Program Exit.\n");
 		exit(-1);
 	}
 	
 	while(list != NULL){
-		removeFirstCharList(list);
+		list = removeFirstCharList(list);
 	}
+}
+
+void mainTest(){
+    CharList* l=createCharList();
+    l->val='o';
+    displayCharList(l);
+    printf("\n%d",getSize(l));
+  
+    appendCharList('e',l);
+    displayCharList(l);
+    printf("\n%d",getSize(l));
+
+    l = addCharList(1,'x',l);
+    displayCharList(l);
+  
+    l = addCharList(2,'y',l);
+    displayCharList(l);
+    printf("\n%d",getSize(l));
+  
+    l = addCharList(-3,'z',l);
+    displayCharList(l);
+    printf("\n%d",getSize(l));
+    
+    l = removeFirstCharList(l);
+    displayCharList(l);
+    printf("\n%d",getSize(l));
+    
+    l = addCharList(3,'u',l);
+    displayCharList(l);
+    printf("\n%d",getSize(l));
+    
+    appendCharList('r',l);
+    displayCharList(l);
+    printf("\n%d",getSize(l));
+    
+    appendCharList('v',l);
+    displayCharList(l);
+    printf("\n%d",getSize(l));
+    
+    appendCharList('t',l);
+    displayCharList(l);
+    printf("\n%d",getSize(l));
+    
+    l = removeLastCharList(l);
+    displayCharList(l);
+    printf("\n%d",getSize(l));
+    
+    destroyCharList(l);
+
 }
--- list.h
+++ list.h
@@ -1,4 +1,66 @@
-typedef struct{
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct _charList CharList;
+struct _charList{
 	char val;
-	struct CharList* next;
-}CharList;
+	CharList* next;
+};
+
+/**
+ * Create a new list of CharList type
+ * return the new list created
+*/
+CharList* createCharList();
+
+/**
+ * Display the given CharList
+*/
+void displayCharList(CharList* list);
+
+/**
+ * return the size of the given CharList
+*/
+int getSize(CharList* list);
+
+/**
+ * Add an element at the given index in the CharList
+ * If the index is negatif, the function start from the end
+ * 
+ * index : the index of the new element
+ * val : the value to put in the cells
+ * list : the CharList
+ * return : the new adress of the CharList if need
+*/
+CharList* addCharList(int index, char val, CharList* list);
+
+/**
+ * Add an element at the end of the CharList
+ * 
+ * val : the value ofthe element
+ * list : the CharList
+*/
+void appendCharList(char val, CharList* list);
+
+/**
+ * Remove the first element in a list
+ * 
+ * list : the CharList
+ * return : the new address of the list
+*/
+CharList* removeFirstCharList(CharList* list);
+
+/**
+ * Remove the last element in a list
+ *
+ * list : The CharList
+ * return : the new address
+*/
+CharList* removeLastCharList(CharList* list);
+
+/**
+ * Remove all the element of a list and free the memory
+ *
+ *list : the list to destroy
+*/
+void destroyCharList(CharList* list);
